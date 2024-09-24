c58f00c6ddb75e87ca2658169000a10c705456d7
miluo16 <miluo@udel.edu>
2024-09-24 14:21:39 -0400

    Fix mongoose warning

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,10 +15,7 @@ app.use(express.json()); // Parse JSON requests
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
